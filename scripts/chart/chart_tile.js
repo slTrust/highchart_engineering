dc3b736fdf497882fc5a6bb6ae093bc235892333
huangjx <trustfor@sina.cn>
2017-11-01 15:44:51 +0800

    delete unuse

--- scripts/chart/chart_tile.js
+++ scripts/chart/chart_tile.js
@@ -23,10 +23,11 @@ function nameSpace(param){
 				var chartTitle = chartJson.chartTitle||'';     //图表名称
 
 				var series = [];
-				var dataClasses = [];
 				if(data&&data.length!=0){
-					var json = this.setX_Y(data.length,chartJson.scale)
-					console.log(json);
+                    var json = this.setX_Y(data.length,chartJson.scale)
+                    console.log('````x   y``````');
+                    console.log(json);
+                    console.log('``````````');
 					//获得纵轴最大值
 					var x = json.x;
 					//获得横轴最大值
@@ -62,64 +63,49 @@ function nameSpace(param){
 						'#dfdfdf',
 						'#b3b3b3',
 						'#979797'
-						];	
-					var color_arr1= [] ;
+                        ];	
+                    // 初始化  类别的颜色
+					var color_arr= [] ;
 					if(chartJson&&chartJson.colorArr&&chartJson.colorArr.length!=0){
-						color_arr1 = chartJson.colorArr;
+						color_arr = chartJson.colorArr;
 					}else{
-						color_arr1 = const_color;
-					}
-					var idx =0;
-					//初始化  类别的颜色
-					
+						color_arr = const_color;
+                    }
+                    // 初始化类别数组 和 series类别
+					var categoryArr =[];
 					for(var name in categoryJson){
-						categoryJson[name]=idx;
-						//
-						dataClasses.push({
-                                from: idx,
-                                to:idx,
-                                color: const_color[idx%color_arr1.length],
-                                name: name
-						});
-						idx++;
+                        categoryArr.push(name);
+                        // 划分类别
+                        series.push({
+                            name:name,
+                            data:[]
+                        });
 					}
-					
-					//横纵坐标
-					series.push({
-						name:'',
-						data:[]
-					});
+                   
+					var that = this;
 					for(var i=0;i<count;i++){
-						//x代表纵轴方向  y代表横轴
-
-						categoryKey = data[i][column2];
-						categoryValue = categoryJson[categoryKey];
-
-						series[0].data.push(
+                        // 获取当前类别
+                        categoryKey = data[i][column2];
+                        // 寻找类别 索引
+                        var idx = that.findInArrIndex(categoryKey,categoryArr);
+                        //x代表纵轴方向  y代表横轴   左上角是原点
+                        series[idx].data.push(
 							{
 								keyId:data[i][column1],
 								name:data[i][column0],
-								dataValue:data[i][column3],
+								category:data[i][column2],
 								x:parseInt(i/y)%x,
 								y:i%y,
-								value:categoryValue  //此value用来 标识种类
+								value:data[i][column3]
 							}
 						)
 					}
 				}
-
-				
-				
-				
-			
-			
-				
-//					container
 					$(containerId).show();
 					$(containerId).highcharts({
 						chart: {
                             type: 'tilemap',
-                            inverted: true,
+                            inverted: true,//xy轴颠倒
                             height: '80%'
 				        },       
 						lang: {
@@ -128,7 +114,8 @@ function nameSpace(param){
 							downloadPDF: '导出 pdf',
 							downloadPNG: '导出 PNG',
 							downloadSVG: '导出 SVG',
-				        },
+                        },
+                        colors:color_arr,
 				        title: {
 				            text: chartTitle,
 				            style: {
@@ -138,6 +125,7 @@ function nameSpace(param){
 				            }
 				        },
 				        xAxis: {
+                            //visible: false,
 				        	//设置轴线颜色  以及宽度
 				        	lineColor: '#999',
 				            lineWidth:2,
@@ -153,15 +141,15 @@ function nameSpace(param){
 				            startOnTick: true,
 				            endOnTick: true,
 				            showLastLabel: true,
-                            gridLineWidth: 1,
-                            max:null
+                            gridLineWidth: 1
 				        },
 				        yAxis: {
+                            //visible: false,
 				        	//设置轴线颜色  以及宽度
 				        	lineColor: '#999',
 				            lineWidth:2,
 				            title: {
-				                text:'2222', //y轴title
+				                text:yTitle, //y轴title
 					            align: 'high',
 				                offset: 0,
 				                rotation: 0,
@@ -169,12 +157,9 @@ function nameSpace(param){
 				            },
                             gridLineWidth: 1
                         },
-                        colorAxis: {
-							 dataClasses:dataClasses //类别数组
-                        },
 				        tooltip: {
                             headerFormat: '',
-                            pointFormat: '<b> {point.name}</b> : <b>{point.dataValue}</b>'
+                            pointFormat: '<b> {point.name}</b> : <b>{point.value}</b>'
                         },
                         plotOptions: {
                             series: {
@@ -188,10 +173,11 @@ function nameSpace(param){
                                 }
                             }
                         },
-				        series: series,
-				        exporting:{
+                        exporting:{
 				        	enabled:true
-				        }
+				        },
+				        series: series
+				       
 					});
 				
 				
